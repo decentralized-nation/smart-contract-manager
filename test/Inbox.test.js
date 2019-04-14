0506b5cb58123be914782226dab85f307c1497e6
Devin Herath <devdhera@gmail.com>
2019-04-14 20:19:44 +0530

    Setup basic provider test

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,3 +2,21 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+
+beforeEach(() => {
+    // Get a List of all accounts
+    web3.eth.getAccounts()
+        .then(fetchedAccounts => {
+            console.log(fetchedAccounts);
+        });
+
+    // Use one of these accounts to deploy
+    // the contract
+
+});
+
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+
+    });
+});
